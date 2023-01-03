771dfaccdbc7253fbc53abea57201ca62bcea2f4
hsklcrry <igeh@yandex.ru>
2023-01-03 22:27:33 +0100

    problem 67 refactoring

--- problem67.hs
+++ problem67.hs
@@ -8,14 +8,6 @@ readInt :: String -> Int
 readInt = read
 
 -- maxOfPrev [a_1, ..., a_n] = [a_1, max(a_1,a_2), ..., max(a_{n-1}, a_n), a_n]
-maxOfPrev as = zipWith0 max 0 (0:as) as
-
-zipWith0 :: (a -> a -> b) -> a -> [a] -> [a] -> [b]
-zipWith0 f d = go
-  where
-    go [] [] = []
-    go [] (b:bs) = f d b : go [] bs
-    go (a:as) [] = f a d : go as []
-    go (x:xs) (y:ys) = f x y : go xs ys
+maxOfPrev as = zipWith max (0:as) (as ++ [0])
 
 ans array = foldr max 0 $ foldl (zipWith (+) . maxOfPrev ) [] array
