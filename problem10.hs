e03d40ca90e6da8f3a04a505f1e795d94f82f469
hsklcrry <igeh@yandex.ru>
2023-01-08 01:37:07 +0100

    problem 10 optimize

--- problem10.hs
+++ problem10.hs
@@ -3,7 +3,7 @@ isPrime :: Integral a => a -> Bool
 isPrime 1 = False
 isPrime n = not (any (\x -> n `mod` x == 0) $ takeWhile (\x -> x*x <= n) $ 2 : [3, 5..])
 
-primes :: [Integer]
+primes :: [Int]
 primes = filter isPrime $ 2 : [3, 5..]
 
 ans = sum $ takeWhile (<2000000) primes 
