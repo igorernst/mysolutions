7c2c84d70acbc0fdf8c9ec867f9578d368d01bfb
hsklcrry <igeh@yandex.ru>
2023-01-08 01:35:51 +0100

    problem 10 done

---
+++ problem10.hs
@@ -0,0 +1,10 @@
+
+isPrime :: Integral a => a -> Bool
+isPrime 1 = False
+isPrime n = not (any (\x -> n `mod` x == 0) $ takeWhile (\x -> x*x <= n) $ 2 : [3, 5..])
+
+primes :: [Integer]
+primes = filter isPrime $ 2 : [3, 5..]
+
+ans = sum $ takeWhile (<2000000) primes 
+main = putStrLn $ show ans
